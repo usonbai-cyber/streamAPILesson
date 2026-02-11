d65e8b1b785f1b5fc5aafa5452dc02597dce9253
Paidalan_techno <usonbayosmonkuluulu@gmail.com>
2026-02-11 10:16:12 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,17 @@
-import java.util.*;
-import java.util.function.Predicate;
- import java.util.Arrays;
- import java.util.List;
- import java.util.stream.Stream;
+public class Main {
+    public static void main(String[] args) {
+        A a = () -> System.out.println("Привет");
 
- import static java.util.Arrays.compare;
- import static java.util.Arrays.stream;
-
- public class Main {
-    public static void main(String[] args){
-        List<Integer> nums = Arrays.asList(3,2,6,9,4,5,7);
-
-
-         nums.stream() //3,2,6,9,4,5,7
-                .filter(n -> n % 2==1)
-                .map(n -> n*3)
-                .forEach(n-> System.out.print(n+ " "));
-
+        a.SayHello();
 
 
 
     }
-
 }
+@FunctionalInterface
+interface A{
+    void SayHello();
+}
+
+
+
